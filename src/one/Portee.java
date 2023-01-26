080423f6ec52c4d5d19f7da31bec9b6444ba25ed
EL MAHFOUD RADOUANE <103253339+aquam503@users.noreply.github.com>
2023-01-26 18:09:31 +0100

    Update Portee.java

--- src/one/Portee.java
+++ src/one/Portee.java
@@ -2,17 +2,15 @@ package one;
 
 public class Portee {
     static int a=5; //variable globale dans toute la classe
-    //fin ma mchit a connu
     public static void main(String[] args) {
         echange();
     }
     public static void echange(){
-        int b=3;    // b variable a portée locale connue
+        int b=3;    
+        // b variable a portée locale connue
         //seulement f method echange
         if(a>0 & b>0) {
-            int temp=a;  //local f l bloc if bra 3la if ca
-            //marche pas ne peut pas etre utilisé bra
-            //3l if
+            int temp=a;
             a=b;
             b=temp;
         }
