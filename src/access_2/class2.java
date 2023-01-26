0671a06e4d06718bf33cbe6c7c1e5506e11adc7c
EL MAHFOUD RADOUANE <103253339+aquam503@users.noreply.github.com>
2023-01-26 18:57:38 +0100

    Update and rename clas2.java to class2.java

--- src/access_2/class2.java
+++ src/access_2/class2.java
@@ -1,9 +1,8 @@
 package access_2;
-//les classes : clas2 et modif ne sont pas dans le meme
-//pack donc on doit faire :
+//les classes : class2 et modif ne sont pas dans le meme pack donc on doit faire :
 import access_1.modif;
 
-public class clas2 {
+public class class2 {
     public static void main(String[] args){
         modif m=new modif();
         int s=m.claculerSomme();
