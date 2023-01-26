aae0c1d71e6c820943174f628611291fafcd5797
EL MAHFOUD RADOUANE <103253339+aquam503@users.noreply.github.com>
2023-01-26 18:55:26 +0100

    Update Main.java

--- src/etudiant/Main.java
+++ src/etudiant/Main.java
@@ -1,10 +1,11 @@
 package etudiant;
 
 public class Main {
-    //zdt static l'objet car sinon on peut pas l'appeler f main
-    //method
-    static Etudiant etd1=new Etudiant("el mahfoud","radouane",1);
-    static Etudiant etd2=new Etudiant("nouali","taha",2);
+    
+    //i used a static object so i can use it in main method
+    
+    static Etudiant etd1=new Etudiant("name","radouane",1);
+    static Etudiant etd2=new Etudiant("name2","firstname2",2);
     public static void main(String[] args){
         Etudiant etd3=new Etudiant("mh","redouane",3);
         System.out.println(etd1.toString());
