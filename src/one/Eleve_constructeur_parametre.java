9970fff86e56f17f5c8ed5b0b2ff99490ed5aef7
EL MAHFOUD RADOUANE <103253339+aquam503@users.noreply.github.com>
2023-01-26 18:05:52 +0100

    Update Eleve_constructeur_parametre.java

--- src/one/Eleve_constructeur_parametre.java
+++ src/one/Eleve_constructeur_parametre.java
@@ -4,13 +4,13 @@ public class Eleve_constructeur_parametre {
     String name;
     int age;
     int level;
-    //on fait pas le type d'envoi dans un constructeur directement public name();
+    //constructor 1
     public Eleve_constructeur_parametre(String n,int a,int l){
         name=n;
         age=a;
         level=l;
     }
-    //un autre constructeur qui vous permet d'initialiser l'objet avec seulement un paramétre
+    // constructor 2
     public Eleve_constructeur_parametre(String n){
         name=n;
     }
