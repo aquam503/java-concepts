a447daea9f5ba37cea7a6651bcd9bb9c8705ea05
EL MAHFOUD RADOUANE <103253339+aquam503@users.noreply.github.com>
2023-01-26 17:22:38 +0100

    Update Chien.java

--- src/Polymorphisme/Chien.java
+++ src/Polymorphisme/Chien.java
@@ -1,8 +1,7 @@
 package Polymorphisme;
 
 public class Chien extends Animal{
-    //ht curseur fo9 marcher et cliquer alt+ctrl
-    //et ratl3lk override
+
     @Override
     public void marcher(){
         System.out.println("Ce chien marche");
