93dd2e2e39a2c7c0cd8985ffb5877b885adcd88c
EL MAHFOUD RADOUANE <103253339+aquam503@users.noreply.github.com>
2023-01-26 17:12:34 +0100

    Update Chien.java

--- src/Heritage3/Chien.java
+++ src/Heritage3/Chien.java
@@ -2,29 +2,23 @@ package Heritage3;
 
 public class Chien extends Animal{
     public Chien(String nom) {
-        /*hna 9bel super ma3ndkch lhe9 dir chi haja*/
+        /*before super don't write anything*/
         super(nom);
     }
     public void marcher(){
         System.out.println(this+" marche.");
     }
-    //Redéfinition d'une méthode katkon liée b héritage
-    //katkon mabin une super classe et sous classe
-    //entre classe parent et classe fille ; we use override
-    //f redéfinition méthode katb9alha meme signature
-    //kant pub tb9a pub; meme type d'envoi;meme nom;meme parametres
+    //Redefinition of a method is bound with inheritance
+    //between a superclass and a subclass
+   //we use override
+   // we keep the same signature
     @Override
     public void manger(){
         System.out.println(this+" mange un os");}
 
-    //Surcharge d'une méthode katkon f la meme classe
-    //daba nmchiw ndiro l surcharge d'une méthode
-    //prenant marcher() méthod
-
-    //f surcharge khsha tkhtalef signature dl méthode
-    //soit tbdl type d'acces ou type de return ou paramétres
-    //pour que java y3tabrha une autre méthode.
-    //ici we change parameter.
+    // overload a method is done in the same class
+   // in the overload we make another signature
+     //for java to consider it another method.
 
     public void marcher(int distance){
         System.out.println(this+" marche "+distance+" km");
