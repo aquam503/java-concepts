aa9be55d0f3ea63f2502e80f45a10db747311671
EL MAHFOUD RADOUANE <103253339+aquam503@users.noreply.github.com>
2023-01-26 17:34:03 +0100

    Update T1.java

--- src/thread_creation/T1.java
+++ src/thread_creation/T1.java
@@ -7,6 +7,6 @@ public class T1 extends Thread{
     }
     //public static void main(String[] args){
     //    T1 obj1=new T1();
-    //    obj1.start(); t9d tdirha hna et runner classe T1; ou tdirha f classe Main bohdaha comme j'ai fait.
+    //    obj1.start(); u can also start your thread here by using just one file.
 
 }
